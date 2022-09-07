f10005b30edb1d33f73136b67a727f689af45fe6
Mehmet Bedir SEVIMLI <mehmetbedirsevimli123@gmail.com>
2022-09-07 17:10:03 +0300

    login form added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm';
+
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+  
+  const [user, setUser] = useState({email: "", password: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if(details.email == adminUser.email && details.password == adminUser.password) {
+    console.log("Logged in");
+    setUser({
+      name: details.name,
+      email:details
+    });
+  } else {
+    console.log("Details do not match!");
+    setError("Hata!");
+  }
+  }
+
+  const Logout = () => {
+    setUser({ email: "", password: ""});
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+        <div>Table</div>
+      ) : (
+        <LoginForm Login={Login} error={error}/>
+      ) }
     </div>
   );
 }
